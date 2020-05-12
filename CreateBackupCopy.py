9b9ee62edbc3a25f63b543ddc135584e10bacfae
Cesar <cesar.caiooliveira@gmail.com>
2020-05-12 14:43:08 -0300

    Update CreateBackupCopy.py

--- CreateBackupCopy.py
+++ CreateBackupCopy.py
@@ -7,14 +7,14 @@ class CreateBackupCopy(sublime_plugin.TextCommand):
             dir_name = os.path.dirname(file_name)
             base_name = os.path.basename(file_name)
             base_base, base_ext = os.path.splitext(base_name)
-            time_stamp = datetime.datetime.now().strftime(" [%Y][%m][%d]  [%H][%M][%S]")
+            time_stamp = datetime.datetime.now().strftime(" [%H][%M][%S]")
             target_dir = os.path.join(dir_name, '.sublime-backup')
             target_file = base_base+time_stamp+base_ext
             target_target = os.path.join(target_dir, target_file)
             os.makedirs(target_dir, exist_ok=True)
-            
-            date_dir = "[%Y][%m][%d]"
+            date_dir = os.path.join(target_dir, datetime.datetime.now().strftime("Data [%Y][%m][%d]"))
             os.makedirs(date_dir, exist_ok=True)
+            target_target = os.path.join(date_dir, target_file)
             try:
                 shutil.copyfile(file_name, target_target)
                 sublime.status_message("Created backup copy %s" % target_file)
